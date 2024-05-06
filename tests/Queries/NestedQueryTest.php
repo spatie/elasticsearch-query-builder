b7f01a131d24277b16a7adca8fca7ae03bc6d86b
dam-bal <d.balandowski@icloud.com>
2024-05-06 20:53:25 +0100

    added test for NestedQuery

---
+++ tests/Queries/NestedQueryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Spatie\ElasticsearchQueryBuilder\Tests\Queries;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use Spatie\ElasticsearchQueryBuilder\Queries\NestedQuery;
+use PHPUnit\Framework\TestCase;
+use Spatie\ElasticsearchQueryBuilder\Queries\Query;
+
+class NestedQueryTest extends TestCase
+{
+    private NestedQuery $nestedQuery;
+
+    protected function setUp(): void
+    {
+        $queryMock = $this->createMock(Query::class);
+
+        $queryMock
+            ->method('toArray')
+            ->willReturn(['query']);
+
+        $this->nestedQuery = new NestedQuery('path', $queryMock);
+    }
+
+    public function testToArrayBuildsCorrectNestedQuery(): void
+    {
+        $this->assertEquals(
+            [
+                'nested' => [
+                    'path' => 'path',
+                    'query' => ['query']
+                ]
+            ],
+            $this->nestedQuery->toArray()
+        );
+    }
+}
