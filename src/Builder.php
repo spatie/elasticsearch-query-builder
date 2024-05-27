b8237f8c535f3f53e7e471c041ae906ca75e629c
Summer <su943515688@gmail.com>
2024-05-27 19:03:32 +0800

    chore: cs-fix

--- src/Builder.php
+++ src/Builder.php
@@ -93,7 +93,7 @@ class Builder
             $params['from'] = $this->from;
         }
 
-        if($this->trackTotalHits) {
+        if ($this->trackTotalHits) {
             $params['track_total_hits'] = true;
         }
 
