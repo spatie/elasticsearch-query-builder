71899fa7889fc33958498e9bc7938660f13c7143
Floris ten Hove <floris@opendeur.nl>
2024-11-15 12:43:53 +0100

    Add more tests

--- tests/Queries/GeoshapeQueryTest.php
+++ tests/Queries/GeoshapeQueryTest.php
@@ -9,6 +9,13 @@ use Spatie\ElasticsearchQueryBuilder\Queries\GeoshapeQuery;
 
 final class GeoshapeQueryTest extends TestCase
 {
+    public function testCreateReturnsNewInstance(): void
+    {
+        $query = GeoshapeQuery::create('location', GeoshapeType::POINT, [1.0, 2.0]);
+
+        self::assertInstanceOf(GeoshapeQuery::class, $query);
+    }
+
     public function testToArrayBuildsCorrectGeoshapeQuery(): void
     {
         $query = GeoshapeQuery::create(
@@ -23,7 +30,6 @@ final class GeoshapeQueryTest extends TestCase
                     [102.0, 2.0],
                 ],
             ],
-            SpatialRelation::WITHIN,
         );
 
         $expected = [
@@ -38,6 +44,68 @@ final class GeoshapeQueryTest extends TestCase
                                 [103.0, 3.0],
                                 [102.0, 3.0],
                                 [102.0, 2.0],
+                            ],
+                        ],
+                    ],
+                    'relation' => 'intersects',
+                ],
+            ],
+        ];
+
+        self::assertEquals($expected, $query->toArray());
+    }
+
+    public function testToArrayBuildsCorrectGeoshapeQueryWithMultiplePolygons(): void
+    {
+        $query = GeoshapeQuery::create(
+            'location',
+            GeoshapeType::MULTI_POLYGON,
+            [
+                [
+                    [
+                        [102.0, 2.0],
+                        [103.0, 2.0],
+                        [103.0, 3.0],
+                        [102.0, 3.0],
+                        [102.0, 2.0],
+                    ],
+                ],
+                [
+                    [
+                        [100.0, 0.0],
+                        [101.0, 0.0],
+                        [101.0, 1.0],
+                        [100.0, 1.0],
+                        [100.0, 0.0],
+                    ],
+                ],
+            ],
+            SpatialRelation::WITHIN,
+        );
+
+        $expected = [
+            'geo_shape' => [
+                'location' => [
+                    'shape' => [
+                        'type' => 'multipolygon',
+                        'coordinates' => [
+                            [
+                                [
+                                    [102.0, 2.0],
+                                    [103.0, 2.0],
+                                    [103.0, 3.0],
+                                    [102.0, 3.0],
+                                    [102.0, 2.0],
+                                ],
+                            ],
+                            [
+                                [
+                                    [100.0, 0.0],
+                                    [101.0, 0.0],
+                                    [101.0, 1.0],
+                                    [100.0, 1.0],
+                                    [100.0, 0.0],
+                                ],
                             ],
                         ],
                     ],
