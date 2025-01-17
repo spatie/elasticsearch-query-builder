79c190cc61056e788ed75ed3df6c7bd3ff60db01
l3aro <dgbao1340@gmail.com>
2025-01-17 17:36:54 +0700

    Correct exception

--- src/Exceptions/InvalidOperatorValue.php
+++ src/Exceptions/InvalidOperatorValue.php
@@ -1,57 +1,13 @@
 <?php
 
-namespace Spatie\ElasticsearchQueryBuilder\Queries;
+namespace Spatie\ElasticsearchQueryBuilder\Exceptions;
 
-use Spatie\ElasticsearchQueryBuilder\Exceptions\InvalidOperatorValue;
+use InvalidArgumentException;
 
-class MatchQuery implements Query
+class InvalidOperatorValue extends InvalidArgumentException
 {
-    protected const VALID_OPERATORS = ['and', 'or'];
-
-    public static function create(
-        string $field,
-        string | int $query,
-        null | string | int $fuzziness = null,
-        null | float $boost = null,
-        null | string $operator = 'or'
-    ): self {
-        return new self($field, $query, $fuzziness, $boost, $operator);
-    }
-
-    public function __construct(
-        protected string $field,
-        protected string | int $query,
-        protected null | string | int $fuzziness = null,
-        protected null | float $boost = null,
-        protected null | string $operator = 'or'
-    ) {
-        if (! in_array(strtolower($operator), self::VALID_OPERATORS)) {
-            throw new InvalidOperatorValue;
-        }
-    }
-
-    public function toArray(): array
+    public function __construct()
     {
-        $match = [
-            'match' => [
-                $this->field => [
-                    'query' => $this->query,
-                ],
-            ],
-        ];
-
-        if ($this->fuzziness) {
-            $match['match'][$this->field]['fuzziness'] = $this->fuzziness;
-        }
-
-        if ($this->boost) {
-            $match['match'][$this->field]['boost'] = $this->boost;
-        }
-
-        if ($this->operator) {
-            $match['match'][$this->field]['operator'] = $this->operator;
-        }
-
-        return $match;
+        parent::__construct('The operator must be either "or" or "and".');
     }
 }
