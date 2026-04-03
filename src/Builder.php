33dfd50bcb881ca22c98d09496a992abdbbe3cc5
Sven Friese <sven.friese@land-in-sicht.de>
2026-04-03 21:39:31 +0200

    Handle all types in "source" method

--- src/Builder.php
+++ src/Builder.php
@@ -173,7 +173,7 @@ class Builder
             return $this;
         }
 
-        $this->fields = array_merge($this->fields ?? [], $fields);
+        $this->fields = array_merge($this->fields ?: [], $fields);
 
         return $this;
     }
